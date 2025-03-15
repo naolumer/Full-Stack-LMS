06182903527d76c74046b73a97955ce376400892
naolumer <naolumer3@gmail.com>
2025-03-15 17:16:49 +0300

    navbar for phone screens

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -10,13 +10,20 @@ const Navbar = () => {
         <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
             <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
             <div className='hidden md:flex items-center gap-5 text-gray-500'>
-                <div>
+                <div className='flex items-center gap-5'>
                     <button type='button'>Become Educator</button>
                     | <Link to="/my-enrollments">My Enrollments</Link>
                 </div>
                 <button type='button' className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
             </div>
-            <div></div>
+            {/* For phone screen */}
+            <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+                <div>
+                    <button>Become Educator</button>
+                    <Link to='/my-enrollments'>My Enrollments</Link>
+                </div>
+                <button><img src={assets.user_icon} alt="" /></button>
+            </div>
         </div>
     );
 }
