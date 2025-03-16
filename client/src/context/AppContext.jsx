d6255afab3db9809f5c5c39735a01f2e990103f4
naolumer <naolumer3@gmail.com>
2025-03-16 14:29:46 +0300

    display courses from dummy courses

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,25 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useState } from "react";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
 
+    // const currency = import.meta.env.VITE_CURRENCY
+    const [allCourses, setAllCourses] = useState([])
+
+    // Fetch aLL cOUrses
+    const fetchAllCourses = async ()=>{
+        setAllCourses(dummyCourses)
+    }
+
+    useEffect(()=>{
+        fetchAllCourses()
+    },[])
+
     const value = {
+        allCourses
 
     }
 
