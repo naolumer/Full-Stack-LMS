4e1afc10bb4c043590f0f47b028a79b0f9a3c391
naolumer <naolumer3@gmail.com>
2025-03-17 21:49:08 +0300

    displaying ratings dynamically

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const CourseCard = ({course}) => {
 
-  const {currency} = useContext(AppContext)
+  const {currency,calculateRating} = useContext(AppContext)
   
   return (
     <Link to={`/course/${course._id}`} onClick={()=>scrollTo(0,0)} 
@@ -15,11 +15,11 @@ const CourseCard = ({course}) => {
           <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
           <p className=' text-gray-500'>Khan Academy</p>
           <div className='flex items-center space-x-2'>
-            <p>4.5</p>
+            <p>{calculateRating(course)}</p>
             <div className='flex'>
-              {[...Array(5)].map((_,i)=>(<img className='w-3.5 h-3.5' key={i} src={assets.star} alt='star'/>))}
+              {[...Array(5)].map((_,i)=>(<img className='w-3.5 h-3.5' key={i} src={i<Math.floor(calculateRating(course)) ? ""} alt='star'/>))}
             </div>
-            <p className='text-gray-500'>(12)</p>
+            <p className='text-gray-500'>{course.courseRatings.length}</p>
           </div>
           <p className='text-base font-semibold text-gray-800'>${(course.coursePrice - course.discount*course.coursePrice/100).toFixed(2)}</p>
         </div>
